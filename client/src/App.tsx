1123609150693e423cbe91a66ae00ef5aacdcd01
Aryanop0914 <21cs038@charusat.edu.in>
2024-01-22 23:49:02 +0530

    Designing Navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './app/Home';
 import Register from './app/Register';
+import Navbar from './app/Navbar';
 
 function App() {
    return (
       <>
          <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/navbar" element={<Navbar />} />
             <Route path="/register" element={<Register />} />
          </Routes>
       </>
---
+++ client/src/app/Navbar.tsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+   return (
+      <div className="flex justify-evenly border-2 p-4">
+         <h1 className=" border-2">Logo</h1>
+         <h2 className="grow  border-2 text-center">Search</h2>
+         <div className="flex justify-evenly border-2">
+            <h2 className=" border-2">home</h2>
+            <h2 className=" border-2">home</h2>
+            <h2 className=" border-2">home</h2>
+         </div>
+      </div>
+   );
+};
+
+export default Navbar;
